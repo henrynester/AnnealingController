577d81d26666983c6be74c8fc45801c7f96861d7
Henry <henrynester@gmail.com>
2022-01-04 08:48:03 -0500

    update for ADS1120 TC Amp

--- AnnealFirmware/src/thermocouple.cpp
+++ AnnealFirmware/src/thermocouple.cpp
@@ -1,141 +1,201 @@
 #include <thermocouple.h>
 #include <SPI.h>
 
-uint8_t Thermocouple::instance_count = 0, Thermocouple::instance_to_update = 0;
-Thermocouple *Thermocouple::instances[2] = {nullptr, nullptr};
+#define SPI_MASTER_DUMMY 0xFF
+// Commands for the ADC
+#define CMD_RESET 0x07
+#define CMD_START_SYNC 0x08
+#define CMD_PWRDWN 0x03
+#define CMD_RDATA 0x1f
+#define CMD_RREG 0x20
+#define CMD_WREG 0x40
 
-void Thermocouple::begin()
+// Configuration registers
+#define CONFIG_REG0_ADDRESS 0x00
+#define CONFIG_REG1_ADDRESS 0x01
+#define CONFIG_REG2_ADDRESS 0x02
+#define CONFIG_REG3_ADDRESS 0x03
+
+// Register masks for setings
+// Register 0
+#define REG_MASK_MUX 0xF0
+#define REG_MASK_GAIN 0x0E
+#define REG_MASK_PGA_BYPASS 0x01
+
+// Register 1
+#define REG_MASK_DATARATE 0xE0
+#define REG_MASK_OP_MODE 0x18
+#define REG_MASK_CONV_MODE 0x04
+#define REG_MASK_TEMP_MODE 0x02
+#define REG_MASK_BURNOUT_SOURCES 0x01
+
+// Register 2
+#define REG_MASK_VOLTAGE_REF 0xC0
+#define REG_MASK_FIR_CONF 0x30
+#define REG_MASK_PWR_SWITCH 0x08
+#define REG_MASK_IDAC_CURRENT 0x07
+
+// Register 3
+#define REG_MASK_IDAC1_ROUTING 0xE0
+#define REG_MASK_IDAC2_ROUTING 0x1C
+#define REG_MASK_DRDY_MODE 0x02
+#define REG_MASK_RESERVED 0x01
+
+uint8_t adc_errcode;
+
+void write_register(uint8_t address, uint8_t value)
 {
-    //chip deselect
-    digitalWrite(cs_pin, HIGH);
-    pinMode(cs_pin, OUTPUT);
-    //initialize SPI object
-    //SPI.begin();
-    pinMode(TC_SCK, OUTPUT);
-    digitalWrite(TC_SCK, LOW);
-    pinMode(TC_MISO, INPUT_PULLUP);
+    SPI.transfer(CMD_WREG | (address << 2));
+    SPI.transfer(value);
 }
 
-float calculate_corrected_temp(float, float);
-
-uint8_t shiftIn()
+uint8_t read_register(uint8_t address)
 {
-    uint8_t value = 0x00;
-    uint8_t i;
-
-    for (i = 0; i < 8; i++)
-    {
-        digitalWrite(TC_SCK, HIGH);
-        delayMicroseconds(5);
-        value |= digitalRead(TC_MISO) << (7 - i);
-        delayMicroseconds(5);
-        digitalWrite(TC_SCK, LOW);
-        delayMicroseconds(10);
-    }
-    return value;
+    SPI.transfer(CMD_RREG | (address << 2));
+    uint8_t data = SPI.transfer(SPI_MASTER_DUMMY);
+    return data;
 }
 
-//MAX31855 datasheet: datasheets.maximintegrated.com/en/ds/MAX31855.pdf
-void Thermocouple::update()
+void send_command(uint8_t command)
 {
-    //bitmap
-    //D31-D18: 14-bit thermocouple temperature data
-    //D17: Reserved
-    //D16: Fault bit
-    //D15-D4: 12-bit internal temperature data
-    //D3: reserved
-    //D2: short circuit to Vcc, D1: to ground, D0: open circuit
+    SPI.transfer(command);
+}
 
-    uint32_t rawData = 0;
-    uint32_t rawInternal = 0;
-    int32_t rawTC = 0;
-
-    digitalWrite(cs_pin, LOW); //select chip
-    delayMicroseconds(10);
-    //SPI.beginTransaction(SPI_SETTINGS); //start SPI comms
-
-    //shift in 4 bytes to the uint32_t rawData
-    rawData = shiftIn(); //SPI.transfer(0);
-    rawData <<= 8;
-    rawData |= shiftIn(); //SPI.transfer(0);
-    rawData <<= 8;
-    rawData |= shiftIn(); //SPI.transfer(0);
-    rawData <<= 8;
-    rawData |= shiftIn(); //SPI.transfer(0);
-
-    SPI.endTransaction();       //stop SPI comms
-    digitalWrite(cs_pin, HIGH); //deselect chip
-
-    errcode = (uint8_t)(rawData & 0b111); //clear any errors from previous calls
-
-    if (rawData == 0)
-    {
-        //could occur when things are OK
-        //for case with no errors, 0degC external, 0degC internal (this last unlikely :)
-        errcode |= 1 << TC_ERR_ALL_ZEROS;
+#define CONFIG_REG_VALS        \
+    {                          \
+        0x0E, 0x00, 0x00, 0x02 \
     }
-    if (rawData == 0xFFFF)
-    {
-        errcode |= 1 << TC_ERR_ALL_ONES;
-    }
+//[0] in+ is AIN0, in- is AIN1, gain=128 (max), internal PGAmp. enabled
+//[1] 20SPS (min for max accuracy), normal mode, single-shot conversion mode, internal temp off, no burnout detection
+//[2] internal 2.048V reference, 60Hz notch disabled (it caused read issues), lowside switch open, excitation sources off
+//[3] excitation sources off, MISO signal is also used to indicate DRDY (data ready) at conversion completion
 
-    //extract the 12-bit internal temperature ADC value
-    rawInternal = rawData >> 4;       //shifts out the D0-D3
-    rawInternal = rawInternal & 4095; //keep only D4-D15
+void adc_init()
+{
+    // pinMode(ADC_MOSI, OUTPUT);
+    // pinMode(ADC_MISO_DRDY, INPUT);
+    // pinMode(ADC_SCK, OUTPUT);
 
-    //Calculate the temperature in Celsius
-    internal_temp = rawInternal * 0.0625; //datasheet page 4 gives this as the degC/LSB
+    // Configure the SPI interface (CPOL=0, CPHA=1)
+    SPI.begin(); //old Arduino.h had pin arguments to .begin(): (ADS1120_CLK_PIN, ADS1120_MISO_PIN, ADS1120_MOSI_PIN);
+    SPI.setDataMode(SPI_MODE1);
 
-    if (internal_temp < 10 || internal_temp > 30)
-    { //room temperature values
-        errcode |= 1 << TC_ERR_INTERNAL_TEMP_WILD;
-    }
+    delay(5);
+    send_command(CMD_RESET); //reset the ADC in case this has not been a hard power cycle of the Arduino 5V bus
+    delay(100);              //wait for device to reboot
 
-    //create the 14-bit (signed) thermocouple ADC value
-    rawTC = (int32_t)(rawData >> 18); //shifts out the D0-D17 to keep only D18-31
-
-    //MSB (14th bit) = 1 means number is negative
-    //14 zeroes -> 0
-    //14 ones -> -1
-    //so subtract 2^14 to convert negative numbers
-    if (rawTC >> 13 == 1)
-    {
-        rawTC -= 16384;
-    }
-
-    //Calculating the temperature in Celsius
-    external_temp_raw = (rawTC)*0.25; //datasheet page 4: 0.25degC/LSB external
-    if (external_temp_raw > 50 || external_temp_raw < -270)
-    { //what should lower bound be?
-        errcode = 1 << TC_ERR_EXTERNAL_TEMP_WILD;
-    }
-
-    //fix linear approximation failure of amplifier
-    calculate_corrected_temp(external_temp_raw, internal_temp);
-
+    const uint8_t reg_vals[] = CONFIG_REG_VALS;
 #ifdef TC_DEBUG
-    Serial.print("DATA: ");
-    Serial.println(rawData, BIN); //print 32-bit raw data in binary format
-    Serial.print("Internal temp: ");
-    Serial.println(internal_temp); //print 12-bit Celsius data
-    Serial.print("Raw thermocouple 14-bit temp: ");
-    Serial.println(rawTC); //print 14-bit raw data
-    Serial.print("Ext temp raw: ");
-    Serial.println(external_temp_raw);
-    Serial.print("errcode:");
-    Serial.println(errcode, HEX);
-    Serial.println();
+    Serial.println("ADC config registers");
 #endif
+    for (uint8_t i = 0; i <= 3; i++)
+    {
+        //set bits in ADC config registers
+        write_register(i, reg_vals[i]);
+        delay(5);
+        //check that each register is set as expected
+        //if not, this indicates a problem with the SPI communication
+        uint8_t read_val = read_register(i);
+        delay(5);
+        if (read_val != reg_vals[i])
+        {
+            adc_errcode |= ADC_ERR_BAD_SPI;
+#ifdef TC_DEBUG
+            Serial.print(i);
+            Serial.println(" INCORRECT");
+#endif
+        }
+#ifdef TC_DEBUG
+        Serial.print(i);
+        Serial.print(":");
+        Serial.println(read_val, HEX);
+#endif
+    }
 }
 
-//ITS-90 coefficients for T-type: www2.me.rochester.edu/courses/ME241/Thermocouple%20conversions.pdf
-inline float calculate_corrected_temp(float external_temp_raw, float internal_temp)
+uint8_t adc_channel;
+void adc_select_channel(uint8_t channel)
 {
-    //coefficient is from datasheet Table 1
-    //assume it's the first sensitivity number
-    //by analogy to formula given for type K thermocouples
-    //this is our best guess at the voltage the amp is receiving
-    float actual_uV = 52.18 * (external_temp_raw - internal_temp);
+    switch (channel)
+    {
+    case ADC_CHANNEL_INTERNAL_TEMP:
+        write_register(CONFIG_REG1_ADDRESS, 0x02); //connect internal temperature sensor to ADC
+        break;
+    case ADC_CHANNEL_TC_A:
+        write_register(CONFIG_REG1_ADDRESS, 0x00); //disconnect internal temperature sensor from ADC
+        write_register(CONFIG_REG0_ADDRESS, 0x0E); //in+ is AIN0, in- is AIN1, gain=128 (max), internal PGAmp enabled
+        break;
+    case ADC_CHANNEL_TC_B:
+        write_register(CONFIG_REG1_ADDRESS, 0x00); //disconnect internal temperature sensor from ADC
+        write_register(CONFIG_REG0_ADDRESS, 0x5E); //in+ is AIN2, in- is AIN3, gain=128 (max), internal PGAmp enabled
+        break;
+    }
+    adc_channel = channel; //remember which channel we're on
+}
+
+uint32_t conversion_start_time; //used to keep track of time waited
+                                //for timeout purposes
+void adc_start_conversion()
+{
+    send_command(CMD_START_SYNC);
+    conversion_start_time = millis();
+}
+
+bool adc_is_conversion_ready()
+{
+    //when the ADC has DRDYM=1 set in the config registers,
+    //the MISO line goes LOW when a conversion is ready for reading.
+
+    //conversion should take 50ms from receiving START/SYNC
+    //if it takes longer, there's an issue
+    if ((millis() - conversion_start_time) > 250)
+    {
+#ifdef TC_DEBUG
+        Serial.println("ADC timed out waiting for conversion");
+#endif
+        adc_errcode |= (1 << ADC_ERR_BAD_SPI); //we probably lost the SPI bus
+        return true;                           //just to break out of the loop
+    }
+    return !digitalRead(ADC_MISO_DRDY);
+}
+
+int16_t adc_read_conversion()
+{
+    if (adc_is_conversion_ready())
+    {
+        //adc reading comes in as two bytes
+        int16_t adcVal = SPI.transfer(SPI_MASTER_DUMMY);
+        adcVal = (adcVal << 8) | SPI.transfer(SPI_MASTER_DUMMY);
+        return adcVal;
+    }
+    else
+    {
+        //function called while DRDY is HIGH, indicating no data ready to read
+        return 0; //idk
+    }
+}
+
+float adc_to_internal_temp(int16_t adc)
+{
+    float temp = (adc >> 2) * 0.03125; //temperature is a left-justified 14-bit value. LSB=0.03125degC
+    //we do not account for the possibility of ADC temperatures below 0degC here
+    if (temp < 3 || temp > 35)
+    { //35-95degF ADC temperatures are reasonable room temperatures
+        adc_errcode |= ADC_ERR_INTERNAL_TEMP_WILD;
+        return 0;
+    }
+    else
+    {
+        adc_errcode &= ~ADC_ERR_INTERNAL_TEMP_WILD;
+        return temp;
+    }
+}
+
+float adc_to_thermocouple_temp(int16_t adc, float internal_temp)
+{
+    //from the datasheet, the LSB=2*Vref/gain/2^16, where Vref=2.048V and gain=128
+    //calculate the thermocouple probe reading, in microvolts
+    float tc_uV = adc * 0.48828125;
 
     //calculate the voltage theoretically developed by a thermocouple
     //at the current reference temp (amp internal temp),
@@ -152,116 +212,73 @@ inline float calculate_corrected_temp(float external_temp_raw, float internal_te
 
     //calculate voltage theoretically developed by a thermocouple at our external
     //temp, but referenced to 0degC
-    float std_ref_uV = actual_uV + ref_uV;
+    float std_tc_uV = tc_uV + ref_uV;
 
     //use the inverse ITS-90 coefficients for type T to get a temperature out
     float external_temp;
-    if (std_ref_uV <= 0)
+    if (std_tc_uV <= 0)
     { //negative temps have one polynomial fit
-        external_temp = 2.5949192E-02 * std_ref_uV +
-                        -2.1316967E-07 * pow(std_ref_uV, 2.0) +
-                        7.9018692E-10 * pow(std_ref_uV, 3.0) +
-                        4.2527777E-13 * pow(std_ref_uV, 4.0) +
-                        1.3304473E-16 * pow(std_ref_uV, 5.0) +
-                        2.0241446E-20 * pow(std_ref_uV, 6.0) +
-                        1.2668171E-24 * pow(std_ref_uV, 7.0);
+        external_temp = 2.5949192E-02 * std_tc_uV +
+                        -2.1316967E-07 * pow(std_tc_uV, 2.0) +
+                        7.9018692E-10 * pow(std_tc_uV, 3.0) +
+                        4.2527777E-13 * pow(std_tc_uV, 4.0) +
+                        1.3304473E-16 * pow(std_tc_uV, 5.0) +
+                        2.0241446E-20 * pow(std_tc_uV, 6.0) +
+                        1.2668171E-24 * pow(std_tc_uV, 7.0);
     }
     else
     { //positive temps have another
-        external_temp = 2.592800E-02 * std_ref_uV +
-                        -7.602961E-07 * pow(std_ref_uV, 2.0) +
-                        4.637791E-11 * pow(std_ref_uV, 3.0) +
-                        -2.165394E-15 * pow(std_ref_uV, 4.0) +
-                        6.048144E-20 * pow(std_ref_uV, 5.0) +
-                        -7.293422E-25 * pow(std_ref_uV, 6.0);
+        external_temp = 2.592800E-02 * std_tc_uV +
+                        -7.602961E-07 * pow(std_tc_uV, 2.0) +
+                        4.637791E-11 * pow(std_tc_uV, 3.0) +
+                        -2.165394E-15 * pow(std_tc_uV, 4.0) +
+                        6.048144E-20 * pow(std_tc_uV, 5.0) +
+                        -7.293422E-25 * pow(std_tc_uV, 6.0);
     }
 #ifdef TC_DEBUG
-    Serial.print("Sensor uV: ");
-    Serial.println(actual_uV);
+    Serial.println("Start TC calc");
+    Serial.print("TC ADC reading: ");
+    Serial.println(adc);
+    Serial.print("TC uV: ");
+    Serial.println(tc_uV);
+    Serial.print("Internal temp: ");
+    Serial.println(internal_temp);
     Serial.print("ref uV: ");
     Serial.println(ref_uV);
     Serial.print("std ref uV: ");
-    Serial.println(std_ref_uV);
+    Serial.println(std_tc_uV);
     Serial.print("corrected temp:");
     Serial.println(external_temp);
+    Serial.println("End TC calc");
 #endif
-    return external_temp;
-}
-
-float Thermocouple::get_raw_temp()
-{
-    return external_temp_raw;
-}
-
-float Thermocouple::get_temp()
-{
-    return external_temp;
-}
-
-float Thermocouple::get_internal_temp()
-{
-    return internal_temp;
-}
-
-uint8_t Thermocouple::get_errcode()
-{
-    return 0; //errcode;
-}
-
-// void Thermocouple::print()
-// {
-//     Serial.print("INT");
-//     Serial.print(internal_temp);
-//     Serial.print("\tEXTRAW");
-//     Serial.print(external_temp_raw);
-//     Serial.print("\tEXTCORR");
-//     Serial.print(external_temp);
-//     Serial.print("\tERR");
-//     Serial.println(errcode, HEX);
-// }
-
-bool Thermocouple::update_all(uint16_t ms)
-{
-    //top of the period to ya
-    if (ms < TC_UPDATE_GAP && instance_to_update == instance_count + 1)
+    if (external_temp < -270 || external_temp > 35)
     {
-        instance_to_update = 0;
+        //just above absolute zero to a hot room temperature is reasonable for the cryogenic apparatus
+        if (adc_channel == ADC_CHANNEL_TC_A)
+        {
+            adc_errcode |= ADC_ERR_TEMP_A_WILD;
+        }
+        else if (adc_channel == ADC_CHANNEL_TC_B)
+        {
+            adc_errcode |= ADC_ERR_TEMP_B_WILD;
+        }
+        return 0;
     }
-    //space updates apart by TC_UPDATE_GAP
-    if (ms > (TC_UPDATE_GAP * instance_to_update))
+    else
     {
-        if (instance_to_update < instance_count)
-        { //still a valid instance number
-//update that thermocouple
-#ifdef TC_TEST_POT
-            instances[instance_to_update]->debug_set_external_temp(
-                map(analogRead((instance_to_update == 0) ? A0 : A1), //A0 is TCA, A1 is TCB
-                    0, 1023, -273, 25));
-#else
-            // if (instance_to_update == 1)
-            // {
-#ifdef TC_DEBUG
-            Serial.print("TC:");
-            Serial.println(instance_to_update);
-#endif
-            //if (instance_to_update == 1)
-            //{
-            instances[instance_to_update]->update();
-            //}
-#endif
-            instance_to_update++;
+        if (adc_channel == ADC_CHANNEL_TC_A)
+        {
+            adc_errcode &= ~ADC_ERR_TEMP_A_WILD;
         }
-        if (instance_to_update == instance_count)
-        { //updated all TCs
-            instance_to_update++;
-            return true;
+        else if (adc_channel == ADC_CHANNEL_TC_B)
+        {
+            adc_errcode &= ~ADC_ERR_TEMP_B_WILD;
         }
+        return external_temp;
     }
-    return false;
 }
 
-void Thermocouple::debug_set_external_temp(float temp)
+uint8_t adc_get_errcode()
 {
-    external_temp = temp;
-    internal_temp = 25; //avoid the error from wild internal temp when we're just debugging
+    return adc_errcode;
 }
